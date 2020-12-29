ec629dec469493a50fe96bda3a54ea9a50cb947e
lawson ekane <lawson.ekane@yahoo.com>
2020-12-28 20:37:26 -0500

    added baltimore to the file

--- bikeshare.py
+++ bikeshare.py
@@ -4,7 +4,7 @@ import numpy as np
 
 CITY_DATA = { 'charlotte': 'chicago.csv',
               'new york city': 'new_york_city.csv',
-              'washington': 'washington.csv' }
+              'washington': 'washington.csv', 'baltimore': 'baltimore.csv'}
 
 def get_filters():
     """
